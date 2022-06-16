d68c7e94aa210d094fa082cd8ad8cb3505345484
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-06-16 15:55:22 +0900

    Initial Commit

---
+++ Streaming/StreamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingApp.swift
+//  Streaming
+//
+//  Created by Russell Carey on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct StreamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
